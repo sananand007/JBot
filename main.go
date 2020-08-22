b959586ca7b5ba2560acd86f051ec906d2be3224
sananand007 <anandjairam@ufl.edu>
2020-08-22 09:12:13 -0700

    Add Get reponse

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"io/ioutil"
+	"net/http"
+	)
+
 
 func main() {
 	fmt.Println("This is the main function..")
+	resp, err := http.Get("https://httpbin.org/get")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
 }
