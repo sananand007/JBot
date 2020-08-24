629e887a2d325c75ad1fe53a741d3486e5311c86
sananand007 <anandjairam@ufl.edu>
2020-08-24 11:39:46 -0700

    Make change token handling [2]

--- build_steps.sh
+++ build_steps.sh
@@ -31,7 +31,7 @@ function post_gitStatus_success() {
 	echo "Post status after build succeeds"
 	git_token=$(extract_token)
 	URL="https://api.GitHub.com/repos/sananand007/JBot/statuses/$GIT_COMMIT?access_token=$git_token"
-	curl -s -H $URL \
+	curl -s -H "$URL" \
   		-H "Content-Type: application/json" \
   		-X POST \
   		-d "{\"state\": \"success\",\"context\": \"continuous-integration/jenkins\", \"description\": \"Jenkins\", \"target_url\": $BUILD_URL}" >> output.txt
